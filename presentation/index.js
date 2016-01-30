e4d4638a7a9c8b414b074ede6057f30b1c477efc
Olivier Tassinari <olivier.tassinari@gmail.com>
2016-01-30 14:07:15 +0100

    [Cours] Présentation entrepreneuriat

--- presentation/index.js
+++ presentation/index.js
@@ -1,146 +1,121 @@
 import React from "react";
 
 import {
-  Appear, BlockQuote, Cite, CodePane, Deck, Fill,
-  Heading, Image, Layout, Link, ListItem, List, Markdown, Quote, Slide, Spectacle, Text
+  Appear,
+  Deck,
+  Heading,
+  Image,
+  Slide,
+  List,
+  Link,
+  ListItem,
+  Spectacle
 } from "../src";
 
 import preloader from "../src/utils/preloader";
 
 import createTheme from "../src/themes/default";
 
-import Interactive from "../assets/interactive";
-
 require("normalize.css");
 require("../src/themes/default/index.css");
 
 const images = {
-  city: require("../assets/city.jpg"),
-  kat: require("../assets/kat.png"),
-  logo: require("../assets/formidable-logo.svg"),
-  markdown: require("../assets/markdown.png")
+  tricount: require("../assets/tricount.png"),
+  splitme: require("../assets/splitme.png")
 };
 
 preloader(images);
 
 const theme = createTheme({
-  primary: "#ff4081"
+  primary: "#4caf50"
 });
 
 export default class Presentation extends React.Component {
   render() {
     return (
       <Spectacle theme={theme}>
-        <Deck transition={["zoom", "slide"]} transitionDuration={500}>
-          <Slide transition={["zoom"]} bgColor="primary">
-            <Heading size={1} fit caps lineHeight={1} textColor="black">
-              Spectacle
+        <Deck transition={["slide"]} transitionDuration={500} progress="bar">
+          <Slide
+            bgColor="primary"
+            notes="Simplifie la vie lorsqu'on pars en voyage entre amis."
+          >
+            <Heading fit lineHeight={1} textColor="secondary" textFont="Roboto">
+              SplitMe
             </Heading>
-            <Heading size={1} fit caps>
-              A ReactJS Presentation Library
+            <Heading fit margin="30px 0px 0px">
+              Partagez vos dépenses entre amis
             </Heading>
-            <Heading size={1} fit caps textColor="black">
-              Where You Can Write Your Decks In JSX
+          </Slide>
+          <Slide
+            transition={["fade"]}
+            bgColor="#C10F37"
+            notes="Pour illustrer le problème, prenons un exemple très simple."
+          >
+            <Heading fit lineHeight={1}>
+              Olivier, Alexandre et Nicolas partant en weekend ski.
             </Heading>
-            <Link href="https://github.com/FormidableLabs/spectacle">
-              <Text bold caps textColor="tertiary">View on Github</Text>
+            <List textColor="white">
+              <Appear><ListItem>Olivier paie 100 pour la location de ski</ListItem></Appear>
+              <Appear><ListItem>Alexandre paie 300 pour l'hôtel</ListItem></Appear>
+              <Appear><ListItem>Nicolas paie 50 pour le dîner</ListItem></Appear>
+            </List>
+            <Appear>
+              <Heading fit>
+                Qui doit combien à qui?
+              </Heading>
+              </Appear>
+          </Slide>
+          <Slide
+            transition={["fade"]}
+            bgColor="#C10F37"
+            notes="Qui veux créez un compte par devises ?"
+          >
+            <Image src={images.tricount.replace("/", "")} margin="0px auto 40px" height={400}/>
+            <Appear>
+              <Heading fit>
+                1 compte par devises
+              </Heading>
+            </Appear>
+          </Slide>
+          <Slide
+            transition={["fade"]}
+            bgColor="#C10F37"
+            notes=""
+          >
+            <Heading fit>
+              Tricount n'est pas éfficace
+            </Heading>
+          </Slide>
+          <Slide
+            notes="Oubliez tout ça, voici SplitMe."
+          >
+            <Link target="_blank" href="https://play.google.com/store/apps/details?id=com.split.app">
+              <Image src={images.splitme.replace("/", "")} />
             </Link>
-            <Text textSize="1.5em" margin="20px 0px 0px" bold>Hit Your Right Arrow To Begin!</Text>
           </Slide>
-          <Slide transition={["slide"]} bgColor="black" notes="You can even put notes on your slide. How awesome is that?">
-            <Image src={images.kat.replace("/", "")} margin="0px auto 40px" height="293px"/>
-            <Heading size={2} caps fit textColor="primary" textFont="primary">
-              Wait what?
-            </Heading>
-          </Slide>
-          <Slide transition={["zoom", "fade"]} bgColor="primary" notes="<ul><li>talk about that</li><li>and that</li></ul>">
-            <CodePane
-              lang="jsx"
-              source={require("raw!../assets/deck.example")}
-              margin="20px auto"
+          <Slide
+            transition={["fade"]}
+            notes=""
+          >
+            <iframe
+              width="100%"
+              height="600px"
+              frameBorder="0"
+              style={{background: "#fff"}}
+              src="https://staging.splitme.net/"
             />
           </Slide>
-          <Slide transition={["slide"]} bgImage={images.city.replace("/", "")} bgDarken={0.75}>
-            <Appear fid="1">
-              <Heading size={1} caps fit textColor="primary">
-                Full Width
-              </Heading>
-            </Appear>
-            <Appear fid="2">
-              <Heading size={1} caps fit textColor="tertiary">
-                Adjustable Darkness
-              </Heading>
-            </Appear>
-            <Appear fid="3">
-              <Heading size={1} caps fit textColor="primary">
-                Background Imagery
-              </Heading>
-            </Appear>
-          </Slide>
-          <Slide transition={["zoom", "fade"]} bgColor="primary">
-            <Heading caps fit>Flexible Layouts</Heading>
-            <Layout>
-              <Fill>
-                <Heading size={4} caps textColor="secondary" bgColor="white" margin={10}>
-                  Left
-                </Heading>
-              </Fill>
-              <Fill>
-                <Heading size={4} caps textColor="secondary" bgColor="white" margin={10}>
-                  Right
-                </Heading>
-              </Fill>
-            </Layout>
-          </Slide>
-          <Slide transition={["slide"]} bgColor="black">
-            <BlockQuote>
-              <Quote>Wonderfully formatted quotes</Quote>
-              <Cite>Ken Wheeler</Cite>
-            </BlockQuote>
-          </Slide>
-          <Slide transition={["spin", "zoom"]} bgColor="tertiary">
-            <Heading caps fit size={1} textColor="primary">
-              Inline Markdown
-            </Heading>
-            <Markdown>
-              {`
-![Markdown Logo](${images.markdown.replace("/", "")})
-
-You can write inline images, [Markdown Links](http://commonmark.org), paragraph text and most other markdown syntax
-* Lists too!
-* With ~~strikethrough~~ and _italic_
-* And let's not forget **bold**
-              `}
-            </Markdown>
-          </Slide>
-          <Slide transition={["slide", "spin"]} bgColor="primary">
-            <Heading caps fit size={1} textColor="tertiary">
-              Smooth
-            </Heading>
-            <Heading caps fit size={1} textColor="secondary">
-              Combinable Transitions
-            </Heading>
-          </Slide>
-          <Slide transition={["fade"]} bgColor="secondary" textColor="primary">
-            <List>
-              <Appear><ListItem>Inline style based theme system</ListItem></Appear>
-              <Appear><ListItem>Autofit text</ListItem></Appear>
-              <Appear><ListItem>Flexbox layout system</ListItem></Appear>
-              <Appear><ListItem>PDF export</ListItem></Appear>
-              <Appear><ListItem>And...</ListItem></Appear>
-            </List>
-          </Slide>
-          <Slide transition={["slide"]} bgColor="primary">
-            <Heading size={1} caps fit textColor="tertiary">
-              Your presentations are interactive
-            </Heading>
-            <Interactive/>
-          </Slide>
-          <Slide transition={["spin", "slide"]} bgColor="tertiary">
-            <Heading size={1} caps fit lineHeight={1.5} textColor="primary">
-              Made with love in Seattle by
-            </Heading>
-            <Link href="http://www.formidable.com"><Image width="100%" src={images.logo}/></Link>
+          <Slide
+            transition={["fade"]}
+            notes=""
+          >
+            <iframe
+              width="100%"
+              height="600px"
+              frameBorder="0"
+              style={{background: "#fff"}}
+              src="http://www.material-ui.com/#/discover-more/showcase"
+            />
           </Slide>
         </Deck>
       </Spectacle>
